8b0ee81f42f17085217946eb40366d900be09f4f
kqueiroz <izaquielqueiroz@live.com>
2016-11-03 21:21:48 -0300

    BD recriado

--- Sprint 8/Diagrama Entidade Relacionamento/CODIGOS_BANCO.sql
+++ Sprint 8/Diagrama Entidade Relacionamento/CODIGOS_BANCO.sql
@@ -1,15 +1,17 @@
+create DATABASE banco_clinica
+
+
 CREATE TABLE PACIENTE (
 
-	 
+	 CPF VARCHAR(15),
      NOME VARCHAR (50) NOT NULL,
-     RG VARCHAR(20) NOT NULL,
-     CPF VARCHAR(20) UNIQUE,
+     RG INT NOT NULL,
      TELEFONE VARCHAR (20) NULL,
-     ESTADO VARCHAR(20) NULL,
-     CIDADE VARCHAR(30) NULL,
-     BAIRRO VARCHAR (30) NULL,
-     RUA VARCHAR (30) NULL,
-     NUMERO INT NULL,
+     ESTADO VARCHAR(30) NULL,
+     CIDADE VARCHAR(40) NULL,
+     BAIRRO VARCHAR (40) NULL,
+     RUA VARCHAR (40) NULL,
+     NUMEROEND INT NULL,
      
      PRIMARY KEY (CPF)
      
@@ -18,43 +20,44 @@ CREATE TABLE PACIENTE (
 
 CREATE TABLE FUNCIONARIO (
 	 
+     CPF VARCHAR(15),
      NOME VARCHAR (50) NOT NULL,
-     RG VARCHAR(20) NOT NULL,
-     CPF VARCHAR(20) UNIQUE,
+     RG INT NOT NULL,
      TELEFONE VARCHAR (20) NULL,
      ESTADO VARCHAR(30) NULL,
-     CIDADE VARCHAR(30) NULL,
-     BAIRRO VARCHAR (30) NULL,
-     RUA VARCHAR (30) NULL,
-     NUMERO INT NULL,
+     CIDADE VARCHAR(40) NULL,
+     BAIRRO VARCHAR (40) NULL,
+     RUA VARCHAR (40) NULL,
+     NUMEROEND INT NULL,
      LOGIN VARCHAR(30) UNIQUE,
      SENHA VARCHAR (30) NULL,
      
      PRIMARY KEY (CPF)
   );
   
+  CREATE TABLE AGENDA(
+	
+    CODIGO INT AUTO_INCREMENT,
+    CPFPACIENTE VARCHAR(11),
+    CPFFUNCIONARIO VARCHAR(11),
+    DATAAGENDAMENTO VARCHAR(10),
+	HORA VARCHAR(10),
+	
+    PRIMARY KEY(CODIGO, CPFPACIENTE,CPFFUNCIONARIO,DATAAGENDAMENTO,HORA),
+    FOREIGN KEY (CPFPACIENTE) REFERENCES PACIENTE (CPF),
+    FOREIGN KEY (CPFFUNCIONARIO) REFERENCES FUNCIONARIO(CPF)
+  );
   
-
-create table agendamento (
-
-	dataConsulta Date,
-    horario varchar(15),
-    nomePaciente varchar(50),
-    nomeFuncionario varchar(50),
-    cpfPaciente varchar (15),
-    cpfFuncionario varchar(15),
-    primary key (dataConsulta, horario, nomePaciente, nomeFuncionario, cpfPaciente, cpfFuncionario),
-    foreign key (cpfPaciente) references paciente(cpf),
-    foreign key (cpfFuncionario) references funcionario(cpf)
-);
-
-
-create table prontuario(
-	cpfPaciente varchar(15),
-    historico varchar(500),
-    dataConsulta Date,
-    horario varchar(15),
+ 
+  
+  CREATE TABLE PRONTUARIO(
+	
+    CODIGO INT AUTO_INCREMENT,
+    CODIGOAGENDA INT,
+	PRONTUARIOAGENDA VARCHAR(1000),
     
-    primary key (cpfPaciente, dataConsulta, horario),
-    foreign key (cpfPaciente) references paciente (cpf)
-);
+    PRIMARY KEY (CODIGO, CODIGOAGENDA),
+    FOREIGN KEY (CODIGOAGENDA) REFERENCES AGENDA(CODIGO)
+  );
+  
+  
